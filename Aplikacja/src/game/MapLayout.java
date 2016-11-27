4f2b71f784014fba937d03c43d78e82bc42fe94f
Grzegorz-00 <grzegorz00new@gmail.com>
2016-11-27 18:47:28 +0100

    Now layout is loaded from text file

--- Aplikacja/src/game/MapLayout.java
+++ Aplikacja/src/game/MapLayout.java
@@ -5,6 +5,12 @@
  */
 package game;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author gregory
@@ -15,6 +21,57 @@ public class MapLayout {
         _width = width;
         _height = heigth;
         _layout = layout;
+    }
+    
+    public MapLayout(String filename)
+    {
+        try
+        {
+            BufferedReader textBufReader = new BufferedReader(new FileReader(filename));
+            String line = textBufReader.readLine();
+            int dataLineNum = 0;
+            while((line = textBufReader.readLine()) != null)
+            {
+                line = line.replaceAll("\\s+", "");
+                if(!line.startsWith("#"))
+                {
+                    if(dataLineNum == 0)
+                    {
+                        String[] fields = line.split(",",3);
+                        if(fields.length != 2)
+                        {
+                            throw new Exception("Wrong data content");
+                        }
+                        _width = Integer.parseInt(fields[0]);
+                        _height = Integer.parseInt(fields[1]);
+                        _layout = new int[_width][_height];
+                    }
+                    else
+                    {
+                        if(dataLineNum > _height + 1)
+                        {
+                            throw new Exception("Wrong data format");
+                        }
+                        String[] fields = line.split(",",_width+1);
+                        if(fields.length != _width)
+                        {
+                            throw new Exception("Wrong data format");
+                        }
+                        for(int i = 0;i<_width;i++)
+                        {
+                            _layout[i][dataLineNum-1] = Integer.parseInt(fields[i]);
+                        }
+                    }
+                    dataLineNum++;
+                }
+            }
+            textBufReader.close();
+        }
+        catch (Exception ex) 
+        {
+            System.err.println("Map file not found");
+        }
+        
     }
     
     public int getWidth()
